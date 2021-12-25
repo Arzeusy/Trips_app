e2d08e568c89fd65800568e61a5eab2f2dc6eb11
arzeusy <jkob1994@gmail.com>
2021-12-24 21:16:47 -0600

    configuracion para coneccion con firebase y modificaciones para complitado para android

---
+++ lib/User/bloc/bloc_user.dart
@@ -0,0 +1,10 @@
+import 'package:generic_bloc_provider/generic_bloc_provider.dart';
+
+class UserBloc implements Bloc {
+  @override
+  void dispose() {
+    // TODO: implement dispose
+  }
+
+
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,9 @@
+// ignore_for_file: import_of_legacy_library_into_null_safe
+
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
+import 'package:generic_bloc_provider/generic_bloc_provider.dart';
+import 'package:platzi_trips_app/User/bloc/bloc_user.dart';
 import 'package:platzi_trips_app/platzi_trips.dart';
 
 
@@ -18,22 +22,25 @@ class MyApp extends StatelessWidget {
       statusBarBrightness: Brightness.light
     )
   );
-    return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
-        primarySwatch: Colors.blue,
-      ),
-      home:  PlatziTrips()
+    return BlocProvider(
+      child: MaterialApp(
+        debugShowCheckedModeBanner: false,
+        title: 'Flutter Demo',
+        theme: ThemeData(
+          // This is the theme of your application.
+          //
+          // Try running your application with "flutter run". You'll see the
+          // application has a blue toolbar. Then, without quitting the app, try
+          // changing the primarySwatch below to Colors.green and then invoke
+          // "hot reload" (press "r" in the console where you ran "flutter run",
+          // or simply save your changes to "hot reload" in a Flutter IDE).
+          // Notice that the counter didn't reset back to zero; the application
+          // is not restarted.
+          primarySwatch: Colors.blue,
+        ),
+        home:  PlatziTrips()
+      ), 
+      bloc: UserBloc()
     );
   }
 }
